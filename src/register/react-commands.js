08b16407d8f6d3a4262fbe1a4c5f0ec5a6ecc47f
Ahmad Karimzade <ahmadkarimzadedev@gmail.com>
2021-06-17 01:05:58 +0430

    fix: react commands function name

--- src/register/react-commands.js
+++ src/register/react-commands.js
@@ -2,7 +2,7 @@ const vscode = require('vscode')
 const create = require('../helpers/createFile')
 const getTemplate = require('../helpers/getTemplate')
 
-const registerVueCommands = (context) => {
+const registerReactCommands = (context) => {
 	const classComponentCommand = vscode.commands.registerCommand('file-creator.react.class-component', (uri) => {
 		const template = getTemplate(/*name*/ 'react-class-component')
 		create(/*target path*/ uri.fsPath, /*format*/ 'js', /*content*/ template)
@@ -17,4 +17,4 @@ const registerVueCommands = (context) => {
 	context.subscriptions.push(functionComponentCommand)
 }
 
-module.exports = registerVueCommands
+module.exports = registerReactCommands
